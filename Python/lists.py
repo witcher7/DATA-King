31ac55fadd007101028819305fc932d3931ac594
Rish <gulati.r7121@gmail.com>
2026-02-02 16:35:28 +0530

    Add list indexing, slicing, and user dict examples
    Expanded lists.py with examples of list indexing, slicing, element modification, and deletion. Also added a sample list of user dictionaries for demonstration purposes.

--- Python/lists.py
+++ Python/lists.py
@@ -7,3 +7,29 @@ user_inputs = [True,'hi', 10.5, 20 ]
 print(len(mylist))
 print(len(myfruits))
 print(len(user_inputs))
+
+## Indexing and Slicing
+print(len(myfruits)) # length of list
+print(myfruits[0])   # Accessing first element
+print(myfruits[-1])  # Accessing last element   
+print(myfruits[0])
+print(myfruits[1:3]) # Slicing from index 1 to 2
+
+# changing elements in list
+myfruits[1] = "mango"
+print(myfruits) # ['apple', 'mango', 'cherry']
+
+# deleting elements from list
+del user_inputs[0]
+print(user_inputs) # [ 'hi', 10.5, 20 ] # it removes first element
+
+users = [
+    {
+        "user": "Alice",
+        "age": 30,
+    }
+    ,{
+        "user": "Rish",
+        "age": 25,
+    }
+]
