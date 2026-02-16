306fe57081194f7d4042239c0894983280a83818
Rish <gulati.r7121@gmail.com>
2026-02-16 19:27:42 +0530

    Update lambda.py

--- Python/lambda.py
+++ Python/lambda.py
@@ -12,3 +12,9 @@ def greeting(greet):
 
 morning_greeting = greeting("Good Morning") # this is now a function
 print(type(morning_greeting))
+
+
+
+## 
+my_nums = [3,4,10,200,-10,-3]
+print(list(filter(lambda num: num%2==0, my_nums)))
