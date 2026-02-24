4ee69f4aa9d79480b889165065fe977c8afce962
Rish <gulati.r7121@gmail.com>
2026-02-24 18:33:46 +0530

    Update date.py

---
+++ Python/date.py
@@ -0,0 +1,9 @@
+import datetime 
+
+print(datetime.MAXYEAR) # max year 
+future_date = datetime.date(2025, 1, 1)
+print(future_date) # date(year, month, day)
+
+print(future_date.year) # year
+print(future_date.month) # month
+print(future_date.day) # day
