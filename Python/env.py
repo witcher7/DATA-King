d924cb39fcd066bea2d2f6352d288f1d1c578ee0
Rish <gulati.r7121@gmail.com>
2026-03-10 13:33:42 +0530

    Create env.py

---
+++ Python/env.py
@@ -0,0 +1,16 @@
+# pip is package manager for python
+
+## pip3 --version
+## pip3 list # to list all the packages installed in your environment
+## pip3 install package_name # to install a package
+## pip3 uninstall package_name # to uninstall a package
+## pip3 show package_name # to show details about a package
+## pip3 install --upgrade package_name # to upgrade a package
+## pip3 freeze > requirements.txt # to create a requirements file with all the installed packages and their versions
+## pip3 install -r requirements.txt # to install all the packages listed in the requirements file
+## pip3 freeze # to list all the installed packages and their versions in the current environment
+
+
+## Python virtual environments are a way to create isolated environments for your Python projects. This allows you to manage dependencies 
+# and avoid conflicts between different projects.
+
