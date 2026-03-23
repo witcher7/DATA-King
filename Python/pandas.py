671bd0bf0fbfb0b6e426b7c6feb43d631ba84027
Rish <gulati.r7121@gmail.com>
2026-03-23 12:39:44 +0530

    Update pandas.py

--- Python/pandas.py
+++ Python/pandas.py
@@ -22,7 +22,7 @@ df.select_dtypes(include='int64') #this will include only those columns which ha
 df.select_dtypes(exclude='int64') #this will include only those columns which has column object
 df['username'].value_counts() # count total columns times 
 df['reputation'].unique() 
-df['username'].sort_values(by="ascending")
+df.sort_values(by="username", ascending=True)
 
 # LOCATION 
 df.loc[2] # label
@@ -30,3 +30,24 @@ df.loc[1:3] # from row 1 to row 3
 df.loc[2:3,['Age',"Total Posts"]]
 df.iloc[3] # index of row 
 df.iloc[4:5]
+
+
+# Filtering 
+df[df['reputation']== 500]
+df[df['Total Posts']>=200]
+df[df['Total Posts']>=200 & (df['Age']>= 30)]
+df[df['Total Posts']>=200 | (df['Age']>= 30)]
+df['reputation'].isin([200,500])
+df[df['Total Posts'].isin(range(200,350))]
+
+## Date and Time range filteration 
+date_range = pd.date_range(start="2032-03-01",end = "2032-08-01")
+df[df['Joined Date'].isin(date_range)]
+
+
+# ADDING new Rows and COlumns 
+df['language'] = 'English' # su
+df['Active'] = [True,False,False] 
+# concatenate 
+# pd.concat([df,df2],axis=1) # will add horizontaly
+df.drop(['language'])
